d6db8945c3c9ed91a838e1daaee4d833a2fabc77
cristian <cristian.mazzarone@stud.unifi.it>
2021-06-24 16:17:42 +0200

    factory commit v1.1

--- cpp/BulletsFactory.cpp
+++ cpp/BulletsFactory.cpp
@@ -6,9 +6,8 @@
 
 std::shared_ptr<Bullet> BulletsFactory::createBullet(EntityType type, sf::Vector2f position, short int direction) {
     std::shared_ptr<Bullet> result(new Bullet(direction));
-    position.y += (16*(float)direction);
+    position.y += 16;
     position.x += (16*(float)direction);
-
     result->load(setBitMap(type), position);
     return result;
 }
